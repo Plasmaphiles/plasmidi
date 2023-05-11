b75726361381b60a191f28f2567bfb272fa1f368
Joe P Shoulak <joe@squarecompass.io>
2023-05-11 00:11:06 -0500

    refactoring

--- server/helpers/process.js
+++ server/helpers/process.js
@@ -23,6 +23,10 @@ const deleteFileCB = file => () =>
   });
 
 const processFile = (res, path) =>
-  runScript("/app/plasmidi.py", sendResult(res, deleteFileCB(path)), path);
+  runScript(
+    "/app/server/scripts/plasmidi.py",
+    sendResult(res, deleteFileCB(path)),
+    path
+  );
 
 module.exports = { processFile };
