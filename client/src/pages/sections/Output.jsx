7fc9bea87d66ffdd2681c96512b91a1a259e1174
Joe P Shoulak <joe@squarecompass.io>
2023-05-12 00:08:32 -0500

    polish

--- client/src/pages/sections/Output.jsx
+++ client/src/pages/sections/Output.jsx
@@ -1,16 +1,13 @@
 import Track from "../../components/Track";
 import Section from "../../components/Section";
 
-const Output = ({ response }) => {
-  return (
-    <Section title="Output">
-      {response ? (
-        response.midi.map(track => <Track track={track} key={track.num} />)
-      ) : (
-        <p>Upload a file</p>
-      )}
-    </Section>
-  );
-};
-
+const Output = ({ response }) => (
+  <Section title="Output">
+    {response ? (
+      response.midi.map(track => <Track track={track} key={track.num} />)
+    ) : (
+      <p>Upload a file</p>
+    )}
+  </Section>
+);
 export default Output;
