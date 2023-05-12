58f9c57c5ff0b1f53d3e0ef19c51ef6813f17ea7
Joe P Shoulak <joe@squarecompass.io>
2023-05-11 22:05:36 -0500

    fixing heroku

--- client/src/components/Track.jsx
+++ client/src/components/Track.jsx
@@ -4,14 +4,16 @@ const Track = ({ track }) => {
   track.name = track.name.match(/[a-zA-Z\s]+/)[0];
   track.notes = JSON.stringify(track.notes).replaceAll("\\", "");
 
+  // FIXME: this is hacky as shit
+  let preview = track.notes.substring(1, 20) + "...";
+  if (preview[0] !== "[") preview = "[" + preview;
+
   return (
     <div style={{ paddingBottom: "10px" }}>
       <span>
         {track.name} - {track.num}:
       </span>
-      <code style={{ display: "inline", padding: "30px" }}>
-        {track.notes.substring(1, 20) + "..."}
-      </code>
+      <code style={{ display: "inline", padding: "30px" }}>{preview}</code>
       <ClipboardButton text={track.notes} />
       <br />
     </div>
