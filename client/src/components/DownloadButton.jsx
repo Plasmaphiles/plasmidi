e5eab2a42f5c7ad183442a7e6d88d9374ad3a49c
Joe P Shoulak <joe@squarecompass.io>
2023-05-11 20:40:47 -0500

    fix button issue

--- client/src/components/DownloadButton.jsx
+++ client/src/components/DownloadButton.jsx
@@ -1,9 +1,8 @@
 const DownloadButton = ({ file, filename }) => (
-  // FIXME: Links extend all the way to the end of the page
   <a
     href={file}
     download={filename}
-    style={{ padding: "10px", display: "block" }}>
+    style={{ margin: "10px", display: "inline-block" }}>
     <button className="btn btn-secondary">{filename}</button>
   </a>
 );
