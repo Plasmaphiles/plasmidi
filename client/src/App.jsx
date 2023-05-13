026349f434119fc6ffe0da382ad60f7ffabe76a3
Joe P Shoulak <joe@squarecompass.io>
2023-05-12 23:52:15 -0500

    raygun analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import rg4js from "raygun4js";
 
-// TODO: Switch to react-bootstrap instead of just boostrap
+rg4js("apiKey", "RFXwCYSCxlOV8C8v6cX3g");
+rg4js("enablePulse", true);
 
 const App = () => (
   <Router>
