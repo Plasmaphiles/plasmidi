4b352789a3ffd276cab4108f138fdac52a0e290f
Joe P Shoulak <joe@squarecompass.io>
2023-05-13 15:57:22 -0500

    checking something on heroku

--- client/src/helpers/copy.js
+++ client/src/helpers/copy.js
@@ -1,6 +1,7 @@
 const copy = text =>
-  new Promise((resolve, reject) =>
-    navigator.clipboard.writeText(text).then(resolve, reject).catch(reject)
+  new Promise(
+    (resolve, reject) =>
+      navigator.clipboard.writeText(text).then(resolve, reject).catch(reject) // not working on heroku?
   );
 
 export default copy;
