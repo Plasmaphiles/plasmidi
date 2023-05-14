317e92c429356a95cc822ffae70e4362a471637b
Joe P Shoulak <joe@squarecompass.io>
2023-05-14 00:48:18 -0500

    polishing zipPlasmidi

--- client/zipPlasmidi.js
+++ client/zipPlasmidi.js
@@ -1,20 +1,28 @@
 const fs = require("fs");
 const archiver = require("archiver");
 
-const zipPath = "src/plasmidi.zip";
+const deleteFile = path => {
+  try {
+    fs.unlinkSync(path);
+    console.log("Zip deleted successfully.");
+  } catch (err) {
+    console.log("error");
+    if (err.code !== "ENOENT") throw err; // File not found is okay
+  }
+};
 
-try {
-  fs.unlinkSync(zipPath);
-  console.log("Zip deleted successfully");
-} catch (err) {
-  if (err.code !== "ENOENT") throw err; // File not found is okay
-}
+const createZip = (zipPath, dirPath) => {
+  const output = fs.createWriteStream(zipPath);
+  const archive = archiver("zip");
 
-const output = fs.createWriteStream(zipPath);
-const archive = archiver("zip");
+  archive.pipe(output);
+  archive.directory(dirPath, false);
+  archive.finalize();
 
-archive.pipe(output);
-archive.directory("../server/scripts", false);
-archive.finalize();
+  output.on("close", () => console.log("Zip file created successfully."));
+};
 
-output.on("close", () => console.log("Zip file created successfully."));
+const zipPath = "client/src/plasmidi.zip";
+
+deleteFile(zipPath);
+createZip(zipPath, "../server/scripts");
