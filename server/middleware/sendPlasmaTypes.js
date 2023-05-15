8ef10f3b8621163e76690aaa3dfe4de76e583ea7
Joe P Shoulak <joe@squarecompass.io>
2023-05-15 13:46:17 -0500

    sendPlasmaTypes includes msg

--- server/middleware/sendPlasmaTypes.js
+++ server/middleware/sendPlasmaTypes.js
@@ -1,10 +1,10 @@
 const sendPlasmaTypes = (_req, res, next) => {
   res.sendPlasMIDI = function (plasMIDI) {
-    res.send({ plasMIDI });
+    res.send({ plasMIDI, msg: res.statusCode === 200 ? "Success" : "Error" });
   };
 
   res.sendPlasmaUI = function (plasmaUI) {
-    res.send({ plasmaUI });
+    res.send({ plasmaUI, msg: res.statusCode === 200 ? "Success" : "Error" });
   };
 
   next();
