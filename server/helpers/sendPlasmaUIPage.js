5e12900796807d1c53118129d7f14ef66fd756c6
Joe P Shoulak <joe@squarecompass.io>
2023-05-14 23:26:25 -0500

    dev/prod fix

--- server/helpers/sendPlasmaUIPage.js
+++ server/helpers/sendPlasmaUIPage.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
+const { isProd } = require("../../isProd");
 
 const sendPlasmaUIPage = (name, res) =>
-  fs.readFile(`./pages/${name}.json`, "utf8", (err, txt) =>
-    err
-      ? res.status(500).send({ msg: "Error loading json" })
-      : res.status(200).send({ page: JSON.parse(txt) })
+  fs.readFile(
+    `${isProd("app/server/", "./")}pages/${name}.json`,
+    "utf8",
+    (err, txt) =>
+      err
+        ? res.status(500).send({ msg: "Error loading json" })
+        : res.status(200).send({ page: JSON.parse(txt) })
   );
 
 module.exports = { sendPlasmaUIPage };
