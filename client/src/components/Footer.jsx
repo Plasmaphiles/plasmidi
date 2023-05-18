9b23e463ffc46c78989d6272555f961232c209db
Joe P Shoulak <joe@squarecompass.io>
2023-05-17 23:50:21 -0500

    added todo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+// TODO: Fix footer styling so it's always at the bottom of the page
 const Footer = ({ children }) => (
   <footer className="bg-primary text-light text-center py-3">
     <h5 className="mb-0">{children}</h5>
