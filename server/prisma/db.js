a525c7ea9667f624fabaa83fc35c331daa10120e
Ethan Bettenga <ethan@listenersoncall.com>
2023-05-28 21:00:56 -0500

    touch ups to get routes working again

--- server/prisma/db.js
+++ server/prisma/db.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 
 const db = new PrismaClient({
-  datasources: { db: { url: process.env.MONGODB_URL } },
+  datasources: { db: { url: process.env.DATABASE_URL } },
 });
 
 module.exports = { db };
--- server/services/users.js
+++ server/services/users.js
@@ -2,6 +2,6 @@ const { db } = require("../prisma/db");
 
 const addUser = (user) => db.user.create({ data: { ...user } });
 
-const getUser = (id) => db.user.findUnique({ where: { id } });
+const getUser = (id) => db.user.findUnique({ where: { id: parseInt(id) } });
 
 module.exports = { addUser, getUser };
