7ab2e2c63531eafce15edd5a97f269bc1746d40d
Zachary Westerman <westerman.zachary@gmail.com>
2023-05-30 18:10:10 -0500

    Clean up instrument mapping

--- server/scripts/convert/instrument.py
+++ server/scripts/convert/instrument.py
@@ -1,15 +1,10 @@
 __all__ = ['to_plasma']
 
 __ID_LIST = {
-	# 'Drum/Kick': [35, 36],
-	# 'Drum/Snare': [38, 40],
-	# 'Drum/HH': [42, 46, 51, 52, 53, 54, 55, 59],
-	# 'Drum/Crash': [49, 57],
-	# 'Drum/Tom1': [48, 50, 60, 62, 63, 65, 67] + range(112, 119),
-	# 'Drum/Tom2': [41, 43, 45, 47, 61, 64, 66, 68],
+	'Drum': [range(9, 17)],
 	'Bass': [range(32, 40)],
-	'Keys': [range(0, 32)] + [range(40, 80)],
 	'Pads': [range(80, 104)],
+	#Everything else is Keys
 }
 
 #Generate reverse instrument list for fast lookup.
